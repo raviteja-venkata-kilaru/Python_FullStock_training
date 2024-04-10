3ac02be7095ce68515252a08e144c9391c6c5031
raviteja-venkata-kilaru <kilaruvenkata.raviteja@randstaddigital.com>
2024-04-10 16:30:50 +0530

    Fetch user Data code

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function fetchUserData() {
+function fetchUserData(data) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        const data = { id: 1, name: "Ravi", email: "Ravi@gmail.com" };
         resolve(data);
       }, 2000);
     });
   }
-async function displayUserData() {
+async function displayUserData(data) {
   try {
-    const userData = await fetchUserData();
+    const userData = await fetchUserData(data);
     console.log("Name:", userData.name);
     console.log("Email:", userData.email);
   } catch (error) {
     console.error("Error fetching user data:", error);
   }
 }
-  
-displayUserData();
+const data = { id: 1, name: "Ravi", email: "Ravi@gmail.com" };
+displayUserData(data);
